2a5f24737e636ca28095bcc54a299515cf25120e
dnx04 <86938955+dnx04@users.noreply.github.com>
2024-12-12 12:21:12 +0700

    update palindrome tree to be more usable

--- strings/PalindromeTree.h
+++ strings/PalindromeTree.h
@@ -1,60 +1,75 @@
-/*
--> cnt contains the number of palindromic suffixes of the node
-*/
+template <int MAXC = 26>
 struct PalindromicTree {
-  struct node {
-    int nxt[26], len, st, en, link, cnt, oc;
-  };
-  string s;
-  vector<node> t;
-  int sz, last;
-  PalindromicTree() {}
-  PalindromicTree(string _s) {
-    s = _s;
-    int n = s.size();
-    t.clear();
-    t.resize(n + 9);
-    sz = 2, last = 2;
-    t[1].len = -1, t[1].link = 1;
-    t[2].len = 0, t[2].link = 1;
+  PalindromicTree(const string& str) : _sz(str.size() + 5), next(_sz, vector<int>(MAXC, 0)), link(_sz, 0), qlink(_sz, 0), cnt(_sz, 0), right_id(_sz, 0), len(_sz, 0), s(_sz, 0) {
+    init();
+    for (int i = 0; i < (int)str.size(); ++i) {
+      add(str[i], i);
+    }
+    count();
   }
-  int extend(int pos) {  // returns 1 if it creates a new palindrome
-    int cur = last, curlen = 0;
-    int ch = s[pos] - 'a';
-    while (1) {
-      curlen = t[cur].len;
-      if (pos - 1 - curlen >= 0 && s[pos - 1 - curlen] == s[pos]) break;
-      cur = t[cur].link;
+  int _sz;
+
+  // returns vector of (left, right, frequency)
+  vector<tuple<int, int, int>> get_palindromes() {
+    vector<tuple<int, int, int>> res;
+    dfs(0, res);
+    dfs(1, res);
+    return res;
+  }
+
+  void dfs(int u, vector<tuple<int, int, int>>& res) {
+    if (u > 1) {  // u = 0 and u = 1 are two empty nodes
+      res.emplace_back(right_id[u] - len[u] + 1, right_id[u], cnt[u]);
     }
-    if (t[cur].nxt[ch]) {
-      last = t[cur].nxt[ch];
-      t[last].oc++;
-      return 0;
+    for (int i = 0; i < MAXC; ++i) {
+      if (next[u][i]) dfs(next[u][i], res);
     }
-    sz++;
-    last = sz;
-    t[sz].oc = 1;
-    t[sz].len = t[cur].len + 2;
-    t[cur].nxt[ch] = sz;
-    t[sz].en = pos;
-    t[sz].st = pos - t[sz].len + 1;
-    if (t[sz].len == 1) {
-      t[sz].link = 2;
-      t[sz].cnt = 1;
-      return 1;
+  }
+
+  int last, n, p;
+  vector<vector<int>> next, dlink;
+  vector<int> link, qlink, cnt, right_id, len, s;
+
+  int newnode(int l, int right) {
+    len[p] = l;
+    right_id[p] = right;
+    return p++;
+  }
+  void init() {
+    p = 0;
+    newnode(0, -1), newnode(-1, -1);
+    n = last = 0;
+    s[n] = -1, link[0] = 1;
+  }
+  int getlink(int x) {
+    while (s[n - len[x] - 1] != s[n]) {
+      if (s[n - len[link[x]] - 1] == s[n])
+        x = link[x];
+      else
+        x = qlink[x];
     }
-    while (1) {
-      cur = t[cur].link;
-      curlen = t[cur].len;
-      if (pos - 1 - curlen >= 0 && s[pos - 1 - curlen] == s[pos]) {
-        t[sz].link = t[cur].nxt[ch];
-        break;
+    return x;
+  }
+  void add(char c, int right) {
+    c -= 'a';
+    s[++n] = c;
+    int cur = getlink(last);
+    if (!next[cur][(int)c]) {
+      int now = newnode(len[cur] + 2, right);
+      link[now] = next[getlink(link[cur])][(int)c];
+      next[cur][(int)c] = now;
+      if (s[n - len[link[now]]] == s[n - len[link[link[now]]]]) {
+        qlink[now] = qlink[link[now]];
+      } else {
+        qlink[now] = link[link[now]];
       }
     }
-    t[sz].cnt = 1 + t[t[sz].link].cnt;
-    return 1;
+    last = next[cur][(int)c];
+    cnt[last]++;
   }
-  void calc_occurrences() {
-    for (int i = sz; i >= 3; i--) t[t[i].link].oc += t[i].oc;
+  void count() {
+    for (int i = p - 1; i >= 0; i--) {
+      cnt[link[i]] += cnt[i];
+    }
   }
-} t;
+};
