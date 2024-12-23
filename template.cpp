b85fc9b2c5bf8687b6923fdcdaad7bfd0280be38
dnx04 <86938955+dnx04@users.noreply.github.com>
2024-12-23 23:54:43 +0700

    lmao

--- template.cpp
+++ template.cpp
@@ -1,33 +1,26 @@
-#include <bits/extc++.h>
+#include <bits/stdc++.h>
 
-#include <tr2/dynamic_bitset>
+#ifdef LOCAL
+#include "tools/prettyprint.hpp"
+#endif
 
 using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-using namespace tr2;
-
-template <typename T>
-using ordered_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-const int RANDOM =
-    chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-  int operator()(int x) const { return x ^ RANDOM; }
-};
-using fast_map = gp_hash_table<int, int, chash>;
-using bs = dynamic_bitset<u_int64_t>;  // lmao
 
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
-typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-signed main() {
+void solve() {}
+
+int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
+  int tc = 1;
+  // cin >> tc;
+  for (int i = 1; i <= tc; ++i) {
+    solve();
+  }
 }
